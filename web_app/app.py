cf85b36b41fa0f7587ddddca4abe5dd1ef701c01
connorslagle <connor.slagle@colorado.edu>
2020-05-18 17:26:59 -0600

    got it on aws

--- web_app/app.py
+++ web_app/app.py
@@ -8,5 +8,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8105, threaded=True)
+    app.run(host='0.0.0.0', port=8008, threaded=True)
 
